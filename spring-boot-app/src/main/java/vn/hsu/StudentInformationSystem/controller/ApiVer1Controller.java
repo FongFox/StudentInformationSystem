0b8ece09128b533c29740c5723900fcc42eb0130
Tran Gia Nguyen Phong <phong.tgn.coder@gmail.com>
2025-04-16 16:50:37 +0700

    chore-SIS | [BE]-config (trello-w4): Add api controller, update config application properties

---
+++ spring-boot-app/src/main/java/vn/hsu/StudentInformationSystem/controller/ApiVer1Controller.java
@@ -0,0 +1,10 @@
+package vn.hsu.StudentInformationSystem.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1")
+public class ApiVer1Controller {
+
+}
